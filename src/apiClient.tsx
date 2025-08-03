6915200436ddd9c98f9196cb6e6ff0270fd6c88d
Sabyasachi-Seal <yoboy907@gmail.com>
2025-08-03 11:46:00 +0530

    feat: Update apiClient to use environment variable for baseURL

--- src/apiClient.tsx
+++ src/apiClient.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: process.env.API_BASE_URL || "http://localhost:8000",
 });
 
 export default apiClient;
